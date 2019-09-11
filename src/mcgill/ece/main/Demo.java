5f0c5a4276e7fe8b75f4ae9e1a0d927813346c18
QuintusHe <zirui.he@mail.mcgill.ca>
2019-09-12 02:55:13 +0800

    second change

--- src/mcgill/ece/main/Demo.java
+++ src/mcgill/ece/main/Demo.java
@@ -2,5 +2,13 @@ package mcgill.ece.main;
 
 public class Demo {
 
+	public static void main(String arg[]) {
+		System.out.println("Hello World");
+		int abc = divison_method(12, 4);
+		System.out.println(abc);
+	}
 	
+	private static int divison_method(int i, int j) {
+		return i/j;
+	}
 }
